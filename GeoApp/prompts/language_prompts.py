a5640475fbe3fa8d6f422f20c99a0e6498220350
Geograffe <jengsiang.seem@gmail.com>
2024-09-20 19:22:19 +0800

    Add files via upload

--- GeoApp/prompts/language_prompts.py
+++ GeoApp/prompts/language_prompts.py
@@ -1,25 +1,58 @@
 prompts = {
     "English": {
-        "prompt": "Please enter your postal code below:",
+        "prompt": "Please enter the postal code of your return address below:",
         "enter_button": "Submit",
         "error_message": "Invalid postal code. Please try again.",
+        "weather_prompt": "Current Weather Conditions",
+        "weather_station": "Weather Station",
+        "weather": "Weather",
+        "temperature": "Temperature",
+        "feels_like": "Feels Like",
+        "humidity": "Humidity",
+        "wind_speed": "Wind Speed",
+        "wind_direction": "Wind Direction"
     },
     "Malay": {
-        "prompt": "Sila masukkan kod pos anda di bawah:",
+        "prompt": "Sila masukkan kod pos alamat pulang anda di bawah:",
         "enter_button": "Hantar",
         "error_message": "Kod pos tidak sah. Sila cuba lagi.",
+        "weather_prompt": "Keadaan Cuaca Semasa",
+        "weather_station": "Stesen Cuaca",
+        "weather": "Cuaca",
+        "temperature": "Suhu",
+        "feels_like": "Rasa Seperti",
+        "humidity": "Kelembapan",
+        "wind_speed": "Kelajuan Angin",
+        "wind_direction": "Arah Angin"
     },
     "Tamil": {
-        "prompt": "தயவுசெய்து உங்கள் அஞ்சல் குறியீட்டை கீழே உள்ளிடவும்:",
+        "prompt": "தயவுசெய்து உங்கள் திரும்பும் முகவரியின் அஞ்சல் குறியீட்டை கீழே உள்ளிடவும்:",
         "enter_button": "சமர்ப்பிக்கவும்",
         "error_message": "தவறான அஞ்சல் குறியீடு. மீண்டும் முயற்சிக்கவும்.",
+        "weather_prompt": "தற்போதைய காலநிலை நிலை",
+        "weather_station": "காலநிலை நிலையம்",
+        "weather": "காலநிலை",
+        "temperature": "வெப்பநிலை",
+        "feels_like": "உணரப்படுகிறது",
+        "humidity": "ஈரப்பதம்",
+        "wind_speed": "காற்று வேகம்",
+        "wind_direction": "காற்றின் திசை"
     },
     "Chinese": {
-        "prompt": "请在下方输入您的邮政编码：",
+        "prompt": "请在下方输入您的返程地址的邮政编码：",
         "enter_button": "提交",
         "error_message": "无效的邮政编码。请再试一次。",
+        "weather_prompt": "当前天气状况",
+        "weather_station": "气象站",
+        "weather": "天气",
+        "temperature": "气温",
+        "feels_like": "体感温度",
+        "humidity": "湿度",
+        "wind_speed": "风速",
+        "wind_direction": "风向"
     }
 }
+
 
 themes = [
     "communityclubs",
